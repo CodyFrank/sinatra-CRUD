b9c7943c3ccce9b275ab740048a5961209b668b1
Cody Frank <cody.frank30@gmail.com>
2020-06-29 14:30:34 -0400

    sets up minor css and layout/index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < Sinatra::Base
+
+    configure do
+        set :views, 'app/views'
+        set :public_folder, '/public'
+    end
+
     get '/' do
-        "Hello World"
+        erb :index
     end
 end
